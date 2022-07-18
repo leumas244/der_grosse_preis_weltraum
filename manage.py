ce41be2402b30fa2c7928b1441843a50462d1d69
leumas244 <FreddiW.15>
2022-07-18 11:21:38 +0200

    weltraum fast fertig

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'der_grosse_preis.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'der_grosse_preis_weltraum.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
